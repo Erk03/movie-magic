24f59de0b6fcd738aadd7e831fa8b27329713e19
Erk03 <135438152+Erk03@users.noreply.github.com>
2024-01-24 17:36:57 +0200

    Add 404 page

--- src/routes.js
+++ src/routes.js
@@ -5,4 +5,9 @@ const movieController = require('./controllers/movieController');
 router.use(homeController);
 router.use(movieController);
 
+router.get('*', (req, res) => {
+    res.redirect('/404');
+})
+
+
 module.exports = router; 
