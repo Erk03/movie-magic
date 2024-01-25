b1589e69e54111a2f13257f4dd1592c98a60d700
Erk03 <135438152+Erk03@users.noreply.github.com>
2024-01-25 16:35:54 +0200

    Add details page

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,7 +3,7 @@ _id: 1,
 title: 'Fast and furious',
 genre: 'car',
 director: 'erkan Rasim',
-date: '12.11.21',
+year: '12.11.21',
 imageUrl: 'https://m.media-amazon.com/images/M/MV5BMjI0NmFkYzEtNzU2YS00NTg5LWIwYmMtNmQ1MTU0OGJjOTMxXkEyXkFqcGdeQXVyMjMxOTE0ODA@._V1_.jpg',
 rating: '5',
 description: 'mnogo gotin film'}];
@@ -17,3 +17,9 @@ exports.create = (movieData) => {
     movieData._id = movies[movies.length - 1]._id + 1;
     movies.push(movieData);
 }    
+
+
+exports.getOne = (movieId) => {
+    const movie = movies.find(movie => movie._id == movieId);
+    return movie;
+}
