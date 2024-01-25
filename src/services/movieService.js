e122110c0ed88c54b774d26e4819d03b850f94f5
Erk03 <135438152+Erk03@users.noreply.github.com>
2024-01-25 15:33:39 +0200

    Add film

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,5 +1,12 @@
-const movies = [];
+const movies = [{title: 'Fast and furious',
+genre: 'car',
+director: 'erkan Rasim',
+date: '12.11.21',
+imageUrl: 'https://m.media-amazon.com/images/M/MV5BMjI0NmFkYzEtNzU2YS00NTg5LWIwYmMtNmQ1MTU0OGJjOTMxXkEyXkFqcGdeQXVyMjMxOTE0ODA@._V1_.jpg',
+rating: '5',
+description: 'mnogo gotin film'}];
 
 exports.create = (movieData) => {
-movies.push(movieData);
+    
+    movies.push(movieData);
 }    
