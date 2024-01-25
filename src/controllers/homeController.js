9fe207c695f5076e447575114b0db7170f7edcf1
Erk03 <135438152+Erk03@users.noreply.github.com>
2024-01-25 17:25:08 +0200

    Added filtring

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -14,8 +14,10 @@ router.get('/about', (req, res) => {
 });
  
 router.get('/search', (req, res) => {
-    const movies = movieService.getAll();
-    res.render('search', { movies });
+    const {title, genre, year} = req.query
+    const movieResult = movieService.search(title, genre, year);
+
+    res.render('search', { movies: movieResult });
 })
 
 router.all('/404', (req, res) =>{
