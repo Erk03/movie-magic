1bcf9913524661a9df37cf152030b5640886e1a9
Erk03 <135438152+Erk03@users.noreply.github.com>
2024-01-25 16:59:03 +0200

    Add ratings to details page

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -5,7 +5,7 @@ genre: 'car',
 director: 'erkan Rasim',
 year: '12.11.21',
 imageUrl: 'https://m.media-amazon.com/images/M/MV5BMjI0NmFkYzEtNzU2YS00NTg5LWIwYmMtNmQ1MTU0OGJjOTMxXkEyXkFqcGdeQXVyMjMxOTE0ODA@._V1_.jpg',
-rating: '5',
+rating: '3',
 description: 'mnogo gotin film'}];
 
 exports.getAll = () => {
