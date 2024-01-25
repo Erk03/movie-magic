95d89ad8edaee12c11c4e640477c02663f08f605
Erk03 <135438152+Erk03@users.noreply.github.com>
2024-01-25 15:57:10 +0200

    Indicate no movies

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,10 +1,10 @@
-const movies = [ {title: 'Fast and furious',
+const movies = [{title: 'Fast and furious',
 genre: 'car',
 director: 'erkan Rasim',
 date: '12.11.21',
 imageUrl: 'https://m.media-amazon.com/images/M/MV5BMjI0NmFkYzEtNzU2YS00NTg5LWIwYmMtNmQ1MTU0OGJjOTMxXkEyXkFqcGdeQXVyMjMxOTE0ODA@._V1_.jpg',
 rating: '5',
-description: 'mnogo gotin film'},  ];
+description: 'mnogo gotin film'}];
 
 exports.getAll = () => {
     return movies.slice();
