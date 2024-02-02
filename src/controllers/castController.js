43d278e95e9934c35accfea84c81e183728dd09e
Erk03 <135438152+Erk03@users.noreply.github.com>
2024-02-02 17:19:56 +0200

    Add post action for cast create

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 
-router.get('create', (req, res) => {
+router.get('/create', (req, res) => {
     res.render('cast/create');
+});
+
+router.post('/create', (req, res) => {
+    const body = req.body;
+
+    res.redirect('/');
 })
 
 module.exports = router;
