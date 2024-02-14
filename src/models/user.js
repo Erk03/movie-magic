07cdff5b91995a160da109287558e4e95b551d59
Erk03 <135438152+Erk03@users.noreply.github.com>
2024-02-14 20:24:30 +0200

    Add rePass validation

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, MongooseError } = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
@@ -13,10 +13,21 @@ const userSchema = new Schema({
   },
 });
 
+userSchema.virtual("rePassword").set(function (value) {
+  if (value !== this.password) {
+    throw new MongooseError("Passwords must match");
+  }
+  // this.rePassword = value;
+});
+
 userSchema.pre("save", async function () {
   const hash = await bcrypt.hash(this.password, 12);
   this.password = hash;
 });
+
+// userSchema.path("rePassword").validate(function () {
+//   return this.password === this.rePassword;
+// }, "Password missmatch!");
 
 const User = model("User", userSchema);
 
