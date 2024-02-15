8590a1366526cca17dfa96e43688a15252bcb346
Erk03 <135438152+Erk03@users.noreply.github.com>
2024-02-15 15:09:27 +0200

    Generate token for succ login

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,3 +1,28 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("../lib/jwt");
+
+const SECRET = "asdasfasdasd";
 
 exports.register = (userData) => User.create(userData);
+
+exports.login = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Cannot find username or password");
+  }
+
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) {
+    throw new Error("PCannot find username or password");
+  }
+
+  const payload = {
+    _id: user._id,
+    email: user.email,
+  };
+  const token = await jwt.sign(payload, SECRET, { expiresIn: "2h" });
+
+  return token;
+};
